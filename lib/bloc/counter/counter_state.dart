706ee6f4794ae1229b44a1fa7f7dfb03bb7b2755
murugan <jeanmurugan11@gmail.com>
2022-05-07 23:20:15 +0200

    counter application using bloc done.

--- lib/bloc/counter/counter_state.dart
+++ lib/bloc/counter/counter_state.dart
@@ -1,26 +1,29 @@
 import 'package:equatable/equatable.dart';
 
 class CounterState extends Equatable {
-
   final int counter;
-
-  const CounterState({
-    required this.counter,
-  });
 
   factory CounterState.initial() {
     return const CounterState(counter: 0);
   }
 
-  @override
-  List<Object?> get props => [counter];
+  const CounterState({
+    required this.counter,
+  });
 
   @override
   String toString() {
     return 'CounterState{counter: $counter}';
   }
 
-  CounterState copyWith({int? counter}) {
-    return CounterState(counter: counter ?? this.counter);
+  @override
+  List<Object> get props => [counter];
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
   }
 }
